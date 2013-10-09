75ec9dea37fa7f39715dc138a60bb4bfd30e3f13
Venkat Venkataraju <venkat.venkataraju@yahoo.com>
2013-10-08 19:35:22 -0700

    initial commit

---
+++ src/com/example/layoutfest/LayoutFestActivity.java
@@ -0,0 +1,22 @@
+package com.example.layoutfest;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class LayoutFestActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_layout_fest);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.layout_fest, menu);
+		return true;
+	}
+
+}
